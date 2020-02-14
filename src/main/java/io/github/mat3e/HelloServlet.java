f0c5026972bc9f0545c92a3e2faa1f08b1064d71
Vampsas <sandra.szablewska@onet.pl>
2020-02-14 12:34:58 +0100

    Added class Service

--- src/main/java/io/github/mat3e/HelloServlet.java
+++ src/main/java/io/github/mat3e/HelloServlet.java
@@ -14,15 +14,28 @@ import java.io.IOException;
 @WebServlet(name = "Hello", urlPatterns = {"/api/*"})
 public class HelloServlet extends HttpServlet {
     private final Logger logger = LoggerFactory.getLogger(HelloServlet.class);
+    private static final String NAME_PARAM = "name";
+    private HelloService service;
 
+    /**
+     * Servlet container needs it.
+     */
+    @SuppressWarnings("unused")
+    public HelloServlet(){
+        this(new HelloService());
+    }
+    public HelloServlet(HelloService service) {
+        this.service = service;
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         var reqestMap = req.getParameterMap();
-        logger.info("Request got");
-        var map = req.getParameter("name");
+        logger.info("Request got" + req.getParameter("name"));
+        resp.getWriter().write(service.prepareGreeting(req.getParameter(NAME_PARAM)));
+       // var name = req.getParameter("name");
 //         ((map != null) ? (resp.getWriter().write("Hello " + map)) : (resp.getWriter().write("Hello " ));
-        resp.getWriter().write("Hello " + (map != null ? map: "someone"));
+        //resp.getWriter().write("Hello " + (NAME_PARAM != null ? NAME_PARAM : "someone"));
     }
 
 
